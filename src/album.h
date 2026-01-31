050d0aad8d9abd9f22843108a04643c523479170
jean <741jean@gmail.com>
2026-01-31 00:44:14 -0300

    Comentario

--- src/album.h
+++ src/album.h
@@ -9,7 +9,8 @@ struct album {
 };
 
 const int MAX = 5; // Tamanho atual do vetor
-int albums_inseridos = 0; // Quantidade de albums salvos
+int albums_inseridos = 0; // Quantidade de albums salvos  <- este valor é modificado pela funçao registra_album e futuramente pela funçao le_dados
 album acervo[MAX];
 
 #endif
+
