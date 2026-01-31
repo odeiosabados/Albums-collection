4afcfed433a5888525cf2dc772fa85f19b3e6590
jean <741jean@gmail.com>
2026-01-30 21:29:02 -0300

    #include register_tools adicionado

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <fstream>
-#include "menu.h"
+#include "menus.h"
+#include "register_tools.h"
 
 int main() {
 	std::ifstream dados;
 	dados.open("data.txt");
 	if (dados.fail())
 		std::ofstream dados("data.txt");
+	
+	menu_inicial();
 	
 	return 0;
 }
