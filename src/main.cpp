4698fbdaa28b6ddb4026bcbac847e858ddb4895d
jean <741jean@gmail.com>
2026-02-01 16:57:58 -0300

    Update main.cpp
    chamada menu agora é controlado pelo main (anteriormente era de maneira recursiva dentro do menu)

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,26 @@
 #include <fstream>
 #include "menus.h"
 #include "register_tools.h"
+#include "album.h"
 
 int main() {
+	// Leitura dos dados previamente salvos //
 	std::ifstream dados;
 	dados.open("data.txt");
 	if (dados.fail())
-		std::ofstream dados("data.txt");
-	
-	menu_inicial();
+		std::ofstream dados("data.txt"); // Cria um arquivo caso nao exista
+	else {
+		while (getline(dados, colecao[num_alb].nome) && dados >> colecao[num_alb].genero >> colecao[num_alb].ano >> colecao[num_alb].num_musicas) {
+			colecao[num_alb].sera_salvo = 'S';
+			num_alb++;
+			dados.ignore();
+		}
+	}
+	//Aplicacao roda ate ser inserido 0 para sair 
+	char opcao;
+	do {
+		opcao = menu_inicial(opcao);
+	} while (opcao != '0');
 	
 	return 0;
 }
