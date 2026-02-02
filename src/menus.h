f025d607edd9b3af61cdd55425c61d96ba9c27be
jean <741jean@gmail.com>
2026-02-02 19:26:57 -0300

    Update menus.h
    removido um while loop misterioso dentro da funçao menu_inicial

--- src/menus.h
+++ src/menus.h
@@ -7,33 +7,31 @@ using std::cout;
 using std::cin;
 
 char menu_inicial(char opcao) {
-	do {
-		cout << "\n";
-		cout << "PLACEHOLDER / MENU ART / PLACEHOLDER\n\n";
-		cout << "digite: \n";
-		cout << "1 para ver todos os albums\n";
-		cout << "2 para adicionar um album\n";
-		cout << "3 para editar um album\n";
-		cout << "4 para pesquisar um album\n";
-		cout << "0 para sair\n";
-		cin >> opcao;
-		switch (opcao) {
-			case '1':
-				imprime_albums(colecao, num_alb);
-				break;
-			case '2':
-				registra_album(colecao, num_alb);
-				break;
-			case '3':
-				edita_album(colecao, num_alb);
-				break;
-			case '4':
-				pesquisa_album(colecao, num_alb);
-				break;
-			default:
-				cout << "Saindo ...";
-		}
-	} while (opcao != '0');
+	cout << "\n";
+	cout << "PLACEHOLDER / MENU ART / PLACEHOLDER\n\n";
+	cout << "digite: \n";
+	cout << "1 para ver todos os albums\n";
+	cout << "2 para adicionar um album\n";
+	cout << "3 para editar um album\n";
+	cout << "4 para pesquisar um album\n";
+	cout << "0 para sair\n";
+	cin >> opcao;
+	switch (opcao) {
+		case '1':
+			imprime_albums(colecao, num_alb);
+			break;
+		case '2':
+			registra_album(colecao, num_alb);
+			break;
+		case '3':
+			edita_album(colecao, num_alb);
+			break;
+		case '4':
+			pesquisa_album(colecao, num_alb);
+			break;
+		default:
+			cout << "Saindo ...";
+	}
 	return opcao;
 }
 
