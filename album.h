af9f131a7a549c4a3c9a4804e1c6520c56cacdd6
Jean Souza <741jean@gmail.com>
2026-02-04 18:22:11 -0300

    funcao aumenta_colecao agora funciona como esperado

--- album.h
+++ album.h
@@ -12,4 +12,20 @@ int MAX = 10; // Tamanho atual do vetor
 int num_alb = 0; // NUMERO DE ALBUMS SALVOS abreviado  <- este valor é modificado pela funçao registra_album e pela leitura de dados do main.cpp
 album* colecao = new album[MAX];
 
+void aumenta_colecao(album* &colecao, int &MAX, int num_alb) {
+	if (num_alb == MAX) {
+		MAX+=10;
+		album* nova_colecao = new album[MAX];
+		for (int i = 0; i < num_alb; i++) {
+			nova_colecao[i].nome = colecao[i].nome;
+			nova_colecao[i].genero = colecao[i].genero;
+			nova_colecao[i].ano = colecao[i].ano;
+			nova_colecao[i].num_musicas = colecao[i].num_musicas;
+			nova_colecao[i].sera_salvo = colecao[i].sera_salvo;
+		}
+		delete[] colecao;
+		colecao = nova_colecao;
+	}
+}
+
 #endif
